508d5b7b7e145b340a731273b1567a29ba6f296e
Alianym <Alianym@users.noreply.github.com>
2021-07-28 21:57:43 +0800

    Initial PointerBox commit

--- Demo.lua
+++ Demo.lua
@@ -28,6 +28,12 @@ LibTutorial.Example = {
 		tutorialType = LIB_TUTORIAL_TYPE_UI_INFO_BOX, 
 		displayPriority = 1, 
 	},
+	["pointerbox"] = {
+		title = GetString(LIBTUTORIAL_EXAMPLE_TITLE), 
+		text = GetString(LIBTUTORIAL_EXAMPLE_TEXT_LONG),
+		tutorialType = LIB_TUTORIAL_TYPE_UI_INFO_BOX, 
+		displayPriority = 1, 
+	},
 }
 
 LIB_TUTORIAL_EXAMPLE = LibTutorialSetup:New(LibTutorial.Example)
